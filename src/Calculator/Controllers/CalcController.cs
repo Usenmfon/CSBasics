0eb66a37d2942589f9f7875a8874216563f68158
Usenmfon <usenmfonuko@gmail.com>
2021-02-11 21:11:04 +0100

    Added index method

--- src/Calculator/Controllers/CalcController.cs
+++ src/Calculator/Controllers/CalcController.cs
@@ -1,10 +1,21 @@
+using Microsoft.AspNetCore.Mvc;
+
 namespace Calculator.Controllers
 {
-    public CalcController : Controller
+    public class CalcController : Controller
     {
         public IActionResult Index()
         {
-            return view();
+            return View();
+        }
+
+        public IActionResult Index(string firstNumber, string secondNumber)
+        {
+            int fNum = int.Parse(firstNumber);
+            int sNum = int.Parse(secondNumber);
+            int addNum = fNum + sNum;
+            ViewBag.result = addNum;
+            return View();
         }
     }
 }
