034e4711f9c3f001646d644c167bf1d1c14bf1b2
Usenmfon <usenmfonuko@gmail.com>
2021-02-12 21:28:48 +0100

    Added new Controller

---
+++ src/AspNetCoreTodo/AspNetCoreTodo/Controllers/TodoController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCoreTodo.Controllers
+{
+    public class TodoController : Controller
+    {
+        public IActionResult Index()
+        {
+            //Get data from db
+            //put items into model
+            //render a view
+        }
+    }
+}
