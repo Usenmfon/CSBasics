7a31fc3faf0b164de4bbe7694b9abab0d7b7364d
Usenmfon <usenmfonuko@gmail.com>
2021-03-01 23:16:22 +0100

    Added new item

--- src/AspNetCoreTodo/AspNetCoreTodo/Services/FakeTodoItemService.cs
+++ src/AspNetCoreTodo/AspNetCoreTodo/Services/FakeTodoItemService.cs
@@ -56,7 +56,13 @@ namespace AspNetCoreTodo.Services
                 DueAt = DateTimeOffset.Now.AddDays(5)
             };
 
-            return Task.FromResult(new[] { item1, item2, item3, item4, item5, item6, item8 });
+            var item9 = new TodoItem
+            {
+                Title = "Reiterating...",
+                DueAt = DateTimeOffset.Now.AddDays(5)
+            };
+
+            return Task.FromResult(new[] { item1, item2, item3, item4, item5, item6, item8, item9 });
         }
     }
 }
