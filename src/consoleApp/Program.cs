4a81a2a2c39176f203b5d2d804d0aa21f9ed98e3
Usenmfon <usenmfonuko@gmail.com>
2021-04-26 12:50:04 +0100

    program to check leap year

--- src/consoleApp/Program.cs
+++ src/consoleApp/Program.cs
@@ -19,11 +19,26 @@ namespace consoleApp
 
             // int eTime = TimeTable.EndTime();
             // Console.WriteLine($"Time elapsed is: {(eTime - sTime)/1000.0}");
-            string postText = Console.ReadLine();
+            // #region here
+            // string postText = Console.ReadLine();
             
-            Post post = new ();
-            post.Text = postText;
-            post.ShowPost();
-        }        
+            // Post post = new ();
+            // post.Text = postText;
+            // post.ShowPost();
+            // #endregion
+            string continueToRun;
+           do
+           {
+               Console.Write("Enter year: ");
+               int year = int.Parse(Console.ReadLine());
+               IsLeapYear(year);
+               Console.Write("Do you want to continue Y/N ? :");
+               continueToRun = Console.ReadLine();
+           }while(continueToRun != "N");
+        }
+        static void IsLeapYear(int year)
+        {
+            Console.WriteLine(DateTime.IsLeapYear(year));
+        }
     }
 }
